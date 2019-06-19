301e5518c9d8ff5f95f7e1ae41d0437adaafd4e7
Mannar Hielal <mannar.hielal@liip.ch>
2019-06-19 14:11:16 +0200

    delete doublicated static_dir in settings and regenerate static folder

--- holidaydream_project/settings.py
+++ holidaydream_project/settings.py
@@ -142,17 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # wagtail integration settings
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
