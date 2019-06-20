1db723225d6ef39d48d4b87b665570606382de89
Mannar Hielal <mannar.hielal@liip.ch>
2019-06-20 10:25:01 +0200

    add partials and install kanbasu

--- holidaydream_project/web/static/web/js/main.js
+++ holidaydream_project/web/static/web/js/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    if (jQuery) {
-        // jQuery is loaded  
-        alert("JQuery works, Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-});
+// $(document).ready(function () {
+//     if (jQuery) {
+//         // jQuery is loaded  
+//         alert("JQuery works, Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// });
