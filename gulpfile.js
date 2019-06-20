c352ac6189b3e4ad401b1b988b86f8f50ecfcc49
Mannar Hielal <mannar.hielal@liip.ch>
2019-06-20 09:35:05 +0200

    install gulp 3.9

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,33 @@
+(function () {
+    var gulp = require('gulp');
+    var sass = require('gulp-sass');
+    var browserSync = require('browser-sync').create();
+    var exec = require('child_process').exec;
 
-
-const
-    gulp = require('gulp'),
-    del = require('del'),
-    sass = require('gulp-sass');
-browserSync = require('browser-sync').create();
-dir = {
-    src: 'src/',
-    build: 'build/'
-};
-
-gulp.task('clean', () => {
-
-    del([dir.build]);
-    done();
-});
-
-gulp.task("sass", gulp.series(function () {
-    /* other code */
-    return gulp.src('holidaydream_project/web/static/web/sass/**/*.scss')
-        .pipe(sass()).on('error', function (err) {
-            console.log(err.toString());
-            this.emit('end');
-        })
-        .pipe(gulp.dest('holidaydream_project/web/static/web/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-}));
-
-
-gulp.task("browserSync", gulp.series(function () {
-    /* other code */
-    browserSync.init({
-        notify: false,
-        port: 8000,
-        proxy: 'http://127.0.0.1:8000/'
+    gulp.task('sass', function () {
+        return gulp.src('holidaydream_project/web/static/web/sass/**/*.scss')
+            .pipe(sass()).on('error', function (err) {
+                console.log(err.toString());
+                this.emit('end');
+            })
+            .pipe(gulp.dest('holidaydream_project/web/static/web/css'))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
     });
 
-}));
+    gulp.task('browserSync', function () {
+        browserSync.init({
+            notify: false,
+            port: 8000,
+            proxy: 'http://127.0.0.1:8000/'
+        });
+    });
 
+    gulp.task('default', ['browserSync', 'sass'], function () {
+        gulp.watch('holidaydream_project/web/static/web/sass/**/*.scss', ['sass']).on('change', browserSync.reload);
+        gulp.watch('holidaydream_project/web/static/web/js/*.js').on('change', browserSync.reload);
+        gulp.watch('holidaydream_project/web/template/**/*.html').on('change', browserSync.reload);
+    });
 
-gulp.task('default', gulp.series('sass', 'browserSync', (done) => {
-
-    // CSS changes
-    gulp.watch('holidaydream_project/web/static/web/sass/**/*.scss', gulp.series('sass'));
-    gulp.watch('holidaydream_project/web/template/**/*.html');
-    done();
-}));
+})();
