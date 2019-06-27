5f86a4b125cd9b8f1f97195162731237d5acd068
Mannar Hielal <mannar.hielal@liip.ch>
2019-06-27 16:58:24 +0200

    create blogindexpage and blogpage

--- holidaydream_project/web/models.py
+++ holidaydream_project/web/models.py
@@ -5,12 +5,38 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full')
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
         FieldPanel('body', classname="full"),
     ]
