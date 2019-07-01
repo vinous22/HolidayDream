990d0e2e91db95b6fb5f646303cafd4ba0a29807
Mannar Hielal <mannar.hielal@liip.ch>
2019-07-01 12:42:09 +0200

    homepage shows featuredindex and teamindex

--- holidaydream_project/web/models.py
+++ holidaydream_project/web/models.py
@@ -13,15 +13,22 @@ from wagtail.core.blocks import BlockQuoteBlock
 
 class HomePage(Page):
     body = RichTextField(blank=True)
-
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        components = self.get_children()
+        context['featured'] = components.type(FeaturedIndexPage)
+        context['teams'] = components.type(TeamIndexPage)
+        return context
+
+# featured area
+
 
 class FeaturedIndexPage(Page):
     intro = RichTextField(blank=True)
-
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full')
     ]
@@ -37,6 +44,32 @@ class FeaturedPage(Page):
     body = StreamField([
         ('image', ImageChooserBlock(blank=True)),
         ('button', blocks.CharBlock(blank=True)),
+        ('paragraph', blocks.CharBlock(blank=True)),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+# team area
+
+
+class TeamIndexPage(Page):
+    intro = RichTextField(blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full')
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        teamCards = self.get_children().live().order_by('first_published_at')
+        context['teamCards'] = teamCards
+        return context
+
+
+class TeamMemberPage(Page):
+    body = StreamField([
+        ('image', ImageChooserBlock(blank=True)),
         ('paragraph', blocks.CharBlock(blank=True)),
     ])
 
